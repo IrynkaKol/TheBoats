62216070c52110e00d06bfad2cea092b83e92925
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-11-10 16:24:26 +0100

    add invisible password

---
+++ js/invisible.js
@@ -0,0 +1,15 @@
+
+function togglePasswordVisibility() {
+    const inputPass=document.getElementById('inputPassword');
+    const iconPass=document.getElementById('icon-pass');
+    
+    iconPass.addEventListener('click', () => {
+        if(inputPass.getAttribute('type') === "password") {
+            inputPass.setAttribute('type', 'text')
+        } else {
+            inputPass.setAttribute('type', 'password')
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', togglePasswordVisibility);
