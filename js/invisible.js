ee2fadf0fdb368d5b53b45fbb0f1b5559529a487
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-11-12 15:06:05 +0100

    update login form and some styles

--- js/invisible.js
+++ js/invisible.js
@@ -2,12 +2,15 @@
 function togglePasswordVisibility() {
     const inputPass=document.getElementById('inputPassword');
     const iconPass=document.getElementById('icon-pass');
+    const iconVisibl=document.getElementById('')
     
     iconPass.addEventListener('click', () => {
         if(inputPass.getAttribute('type') === "password") {
             inputPass.setAttribute('type', 'text')
+            iconPass.height === '0px'
         } else {
             inputPass.setAttribute('type', 'password')
+            iconPass.height === '14px'
         }
     })
 }
