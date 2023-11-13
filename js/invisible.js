9b92138d77d30d38fe91110c8b2967499b9dfb08
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-11-14 00:02:17 +0100

    update hide show password

--- js/invisible.js
+++ js/invisible.js
@@ -1,17 +1,14 @@
 
-function togglePasswordVisibility() {
-    const inputPass=document.getElementById('inputPassword');
-    const iconPass=document.getElementById('icon-pass');
-    const iconVisibl=document.getElementById('')
-    
-    iconPass.addEventListener('click', () => {
-        if(inputPass.getAttribute('type') === "password") {
-            inputPass.setAttribute('type', 'text')
-            iconPass.height === '0px'
-        } else {
-            inputPass.setAttribute('type', 'password')
-            iconPass.height === '14px'
-        }
-    })
+
+function togglePasswordVisibility(target) {
+  let input = document.getElementById("inputPassword");
+
+  if (input.getAttribute("type") == "password") {
+    target.classList.add("show");
+    input.setAttribute("type", "text");
+  } else {
+    target.classList.remove("show");
+    input.setAttribute("type", "password");
+  }
+  return false;
 }
- document.addEventListener('DOMContentLoaded', togglePasswordVisibility);
---
+++ js/password.js
@@ -0,0 +1,19 @@
+
+function passwordVisibility(passwordId) {
+    let inputPass = document
+      .getElementById(passwordId)
+      .getElementsByClassName('form__input')[0];
+    let hideButton = document.querySelector(`#${passwordId} .hide`);
+  
+    if (inputPass) {
+      if (inputPass.getAttribute("type") === "password") {
+        inputPass.setAttribute("type", "text");
+        hideButton.classList.add("show");
+      } else {
+        inputPass.setAttribute("type", "password");
+        hideButton.classList.remove("show");
+      }
+    }
+  
+    return false;
+  }
