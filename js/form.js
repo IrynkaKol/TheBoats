fa4c6a02828e909d421c3cc441c0ddd58e741428
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-11-18 12:46:28 +0100

    update form js

--- js/form.js
+++ js/form.js
@@ -1,162 +1,193 @@
+const hideableIcons1 = document.querySelectorAll(".hideable-icon-1");
+const hideableIcons2 = document.querySelectorAll(".hideable-icon-2");
+
+hideableIcons1.forEach((icon) => {
+  icon.style.display = "inline-block";
+});
+
+hideableIcons2.forEach((icon) => {
+  icon.style.display = "inline-block";
+});
+
 function showError(elementId, errorMessage) {
-    const errorElement = document.getElementById(elementId);
-    if (errorElement) {
-        errorElement.innerText = errorMessage;
-        errorElement.style.display = 'block';
+  const errorElement = document.getElementById(elementId);
+  if (errorElement) {
+    errorElement.innerText = errorMessage;
+    errorElement.style.display = "block";
 
-        // Отримуємо інпут, для якого відображаємо помилку
-        const inputElement = document.querySelector(`[name=${elementId.replace('-error', '')}]`);
-        if (inputElement) {
-            inputElement.classList.add('error-input');
-        }
-
-        // Показуємо іконку помилки
-        const iconWarning = document.getElementById(`warning-${elementId.replace('-error', '')}`);
-        if (iconWarning) {
-            iconWarning.style.display = 'inline-block';
-        }
-
-        // Ховаємо іконки приховування пароля
-        const hideableIcons = document.querySelectorAll('.hideable-icon');
-        hideableIcons.forEach(icon => {
-            icon.style.display = 'none';
-        });
+    const inputElement = document.querySelector(
+      `[name=${elementId.replace("-error", "")}]`
+    );
+    if (inputElement) {
+      inputElement.classList.add("error-input");
     }
+
+    const iconWarning = document.getElementById(
+      `warning-${elementId.replace("-error", "")}`
+    );
+    if (iconWarning) {
+      iconWarning.style.display = "inline-block";
+    }
+
+    if (
+      elementId.includes("password-error") ||
+      elementId.includes("confirm-error")
+    ) {
+      hideableIcons1.forEach((icon) => {
+        icon.style.display = "none";
+      });
+
+      hideableIcons2.forEach((icon) => {
+        icon.style.display = "none";
+      });
+    }
+  }
 }
 
 function hideError(elementId) {
-    const errorElement = document.getElementById(elementId);
-    if (errorElement) {
-        errorElement.innerText = '';
-        errorElement.style.display = 'none';
+  const errorElement = document.getElementById(elementId);
+  if (errorElement) {
+    errorElement.innerText = "";
+    errorElement.style.display = "none";
 
-        // Отримуємо інпут, для якого приховуємо помилку
-        const inputElement = document.querySelector(`[name=${elementId.replace('-error', '')}]`);
-        if (inputElement) {
-            inputElement.classList.remove('error-input');
-        }
-
-        // Приховуємо іконку помилки
-        const iconWarning = document.getElementById(`warning-${elementId.replace('-error', '')}`);
-        if (iconWarning) {
-            iconWarning.style.display = 'none';
-        }
-
-        // Приховуємо іконки приховування пароля
-        const hideableIcons = document.querySelectorAll('.hideable-icon');
-        hideableIcons.forEach(icon => {
-            icon.style.display = 'none';
-        });
-
-        // Показуємо іконки приховування пароля, якщо немає помилок
-        const allErrors = document.querySelectorAll('.error-message');
-        const hasErrors = [...allErrors].some(error => error.style.display === 'block');
-        if (!hasErrors) {
-            const hideableIcons = document.querySelectorAll('.hideable-icon');
-            hideableIcons.forEach(icon => {
-                icon.style.display = 'inline-block';
-            });
-        }
+    const inputElement = document.querySelector(
+      `[name=${elementId.replace("-error", "")}]`
+    );
+    if (inputElement) {
+      inputElement.classList.remove("error-input");
     }
+
+    const iconWarning = document.getElementById(
+      `warning-${elementId.replace("-error", "")}`
+    );
+    if (iconWarning) {
+      iconWarning.style.display = "none";
+    }
+
+    console.log(hideableIcons1);
+    console.log(hideableIcons2);
+    hideableIcons1.forEach((icon) => {
+      icon.style.display = "inline-block";
+    });
+
+    hideableIcons2.forEach((icon) => {
+      icon.style.display = "inline-block";
+    });
+  }
 }
 
 function validateField(fieldName, fieldValue) {
-    const errorId = `${fieldName}-error`;
+  const errorId = `${fieldName}-error`;
 
-    switch (fieldName) {
-        case 'inputEmail1':
-            const emailError = fieldValue === '' ? 'Field must not be empty' : validateEmail(fieldValue);
-            showError(errorId, emailError);
-            break;
-        case 'inputUsername':
-            const usernameError = fieldValue === '' ? 'Field must not be empty' : validateUsername(fieldValue);
-            showError(errorId, usernameError);
-            break;
-        case 'passwordInput1':
-            const passwordError = fieldValue === '' ? 'Field must not be empty' : validatePassword(fieldValue);
-            showError(errorId, passwordError);
-            break;
-        case 'passwordInput2':
-            const password2 = document.getElementById('passwordInput2').value;
-            const confirmPasswordError = fieldValue === '' ? 'Field must not be empty' : validatePasswordConfirmation(password2, fieldValue);
-            showError(errorId, confirmPasswordError);
-            break;
-        default:
-            break;
-    }
+  switch (fieldName) {
+    case "inputEmail1":
+      const emailError =
+        fieldValue === ""
+          ? "Field must not be empty"
+          : validateEmail(fieldValue);
+      showError(errorId, emailError);
+      break;
+    case "inputUsername":
+      const usernameError =
+        fieldValue === ""
+          ? "Field must not be empty"
+          : validateUsername(fieldValue);
+      showError(errorId, usernameError);
+      break;
+    case "passwordInput1":
+      const passwordError =
+        fieldValue === ""
+          ? "Field must not be empty"
+          : validatePassword(fieldValue);
+      showError(errorId, passwordError);
+      break;
+    case "passwordInput2":
+      const password2 = document.getElementById("passwordInput2").value;
+      const confirmPasswordError =
+        fieldValue === ""
+          ? "Field must not be empty"
+          : validatePasswordConfirmation(password2, fieldValue);
+      showError(errorId, confirmPasswordError);
+      break;
+    default:
+      break;
+  }
 }
 
-document.getElementById('add-form').addEventListener('submit', function (e) {
-    e.preventDefault();
+document.getElementById("add-form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const email = document.getElementById('inputEmail1').value;
-    const username = document.getElementById('inputUsername').value;
-    const password1 = document.getElementById('passwordInput1').value;
-    const password2 = document.getElementById('passwordInput2').value;
+  const email = document.getElementById("inputEmail1").value;
+  const username = document.getElementById("inputUsername").value;
+  const password1 = document.getElementById("passwordInput1").value;
+  const password2 = document.getElementById("passwordInput2").value;
 
-    validateField('inputEmail1', email);
-    validateField('inputUsername', username);
-    validateField('passwordInput1', password1);
-    validateField('passwordInput2', password2);
+  validateField("inputEmail1", email);
+  validateField("inputUsername", username);
+  validateField("passwordInput1", password1);
+  validateField("passwordInput2", password2);
 
-    const emailError = validateEmail(email);
-    if (emailError !== '') {
-        showError('email-error', emailError);
-        return false;
-    } else {
-        hideError('email-error');
-    }
+  const emailError = validateEmail(email);
+  if (emailError !== "") {
+    showError("email-error", emailError);
+    return false;
+  } else {
+    hideError("email-error");
+  }
 
-    const usernameError = validateUsername(username);
-    if (usernameError !== '') {
-        showError('username-error', usernameError);
-        return false;
-    } else {
-        hideError('username-error');
-    }
+  const usernameError = validateUsername(username);
+  if (usernameError !== "") {
+    showError("username-error", usernameError);
+    return false;
+  } else {
+    hideError("username-error");
+  }
 
-    const passwordError = validatePassword(password1);
-    if (passwordError !== '') {
-        showError('password-error', passwordError);
-        return false;
-    } else {
-        hideError('password-error');
-    }
+  const passwordError = validatePassword(password1);
+  if (passwordError !== "") {
+    showError("password-error", passwordError);
+    return false;
+  } else {
+    hideError("password-error");
+  }
 
-    const confirmPasswordError = validatePasswordConfirmation(password1, password2);
-    if (confirmPasswordError !== '') {
-        showError('confirm-error', confirmPasswordError);
-        return false;
-    } else {
-        hideError('confirm-error');
-    }
+  const confirmPasswordError = validatePasswordConfirmation(
+    password1,
+    password2
+  );
+  if (confirmPasswordError !== "") {
+    showError("confirm-error", confirmPasswordError);
+    return false;
+  } else {
+    hideError("confirm-error");
+  }
 
-    // alert('Form is valid');
-    document.getElementById('registration').style.display = 'none';
-    document.getElementById('success-message').style.display = 'block';
+  // alert('Form is valid');
+  document.getElementById("registration").style.display = "none";
+  document.getElementById("success-message").style.display = "block";
 });
 
-const inputs = document.querySelectorAll('.form__input');
+const inputs = document.querySelectorAll(".form__input");
 inputs.forEach((input) => {
-    input.addEventListener('focus', function () {
-        this.setAttribute('placeholder', '');
-    });
+  input.addEventListener("focus", function () {
+    this.setAttribute("placeholder", "");
+  });
 
-    input.addEventListener('blur', function () {
-        if (!this.value.trim()) {
-            this.setAttribute('placeholder', this.getAttribute('data-placeholder'));
-        }
-    });
+  input.addEventListener("blur", function () {
+    if (!this.value.trim()) {
+      this.setAttribute("placeholder", this.getAttribute("data-placeholder"));
+    }
+  });
 
-    input.addEventListener('change', function () {
-        validateField(this.id, this.value);
-    });
+  input.addEventListener("change", function () {
+    validateField(this.id, this.value);
+  });
 });
 
 function validatePasswordConfirmation(password, confirmPassword) {
-    if (password !== confirmPassword) {
-        return 'Passwords do not match';
-    } else {
-        return '';
-    }
+  if (password !== confirmPassword) {
+    return "Passwords do not match";
+  } else {
+    return "";
+  }
 }
