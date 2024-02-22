9c8700e585662a1909168e53d40fc9742b8fa2bc
Iryna Kolomoiec <kolomoyka@gmail.com>
2024-02-22 18:33:40 +0100

    update slider

--- js/slider.js
+++ js/slider.js
@@ -1,50 +1,48 @@
-const images = document.querySelectorAll(' .about__slider .about__slider-line img')
-const sliderLine = document.querySelector('.about__slider-line');
+const images = document.querySelectorAll(
+  " .about__slider .about__slider-line img"
+);
+const sliderLine = document.querySelector(".about__slider-line");
 let count = 0;
 let width;
 
 function init() {
-/*console.log('resize')*/
+  width = document.querySelector(".about__slider").offsetWidth;
 
-width = document.querySelector('.about__slider').offsetWidth;
-
-images.forEach(item =>{
-    //console.log('item', item.offsetWidth);
-    item.style.width = width + 'px';
-    item.style.height = 'auto'
-})
-sliderLine.style.width = width * images.length + 'px';
-rollSlider()
- console.log(width)
+  images.forEach((item) => {
+    item.style.width = width + "px";
+    item.style.height = "auto";
+  });
+  sliderLine.style.width = width * images.length + "px";
+  rollSlider();
+  console.log(width);
 }
 
+window.addEventListener("resize", init);
+window.addEventListener("DOMContentLoaded", init);
+init();
 
- window.addEventListener('resize', init);
- window.addEventListener('DOMContentLoaded', init);
- init ()
-
- document.querySelector('.about__slider-right').addEventListener('click', function(){
-    
+document
+  .querySelector(".about__slider-right")
+  .addEventListener("click", function () {
     count += 1;
     if (count >= images.length) {
-        count = 0
+      count = 0;
     }
 
-    rollSlider()
+    rollSlider();
+  });
 
- });
-
- document.querySelector('.about__slider-left').addEventListener('click', function(){
-    
+document
+  .querySelector(".about__slider-left")
+  .addEventListener("click", function () {
     count -= 1;
     if (count < 0) {
-        count = images.length - 1
+      count = images.length - 1;
     }
 
-    rollSlider()
+    rollSlider();
+  });
 
- });
-
- function rollSlider() {
-sliderLine.style.transform = 'translate(-' + count * width +'px)'
- } 
+function rollSlider() {
+  sliderLine.style.transform = "translate(-" + count * width + "px)";
+}
